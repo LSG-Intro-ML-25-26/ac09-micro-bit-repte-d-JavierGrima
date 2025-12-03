f9c3909c0918b29e856cdfb2ea129d916f14485b
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2025-12-03 20:32:07 +0100

    Temperatura y nivel de luz

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("fred")
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("G F G A - F E D ", 120), music.PlaybackMode.UntilDone)
+    } else {
+        music.stopAllSounds()
+    }
 })
